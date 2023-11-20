fab0820f833a7eea92a07569075312c457527ade
siddharth51292 <siddharth.avadhanam@gmail.com>
2023-11-20 12:42:27 -0500

    tracked haplotypein gco bp output

--- gcosim.py
+++ gcosim.py
@@ -254,6 +254,7 @@ class _Node():
                 for d in l:
                     if d['gco']:
                         d['chr'] = c
+                        d['hap'] = h
                         gco_dict_list.append(d)
         self.df_gco=pd.DataFrame(gco_dict_list)
 
